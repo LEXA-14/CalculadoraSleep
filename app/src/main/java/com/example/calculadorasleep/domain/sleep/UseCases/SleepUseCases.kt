e9085eb1458886fdb32a4bfcc0c1344d032b8456
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-08-09 17:12:03 -0400

    Fix: Hace que el calculo de promedio se recalcula cada vez que se anade un nuevo dato

--- app/src/main/java/com/example/calculadorasleep/domain/sleep/UseCases/SleepUseCases.kt
+++ app/src/main/java/com/example/calculadorasleep/domain/sleep/UseCases/SleepUseCases.kt
@@ -5,6 +5,8 @@ import com.example.calculadorasleep.domain.sleep.Validation.validateTiempos
 import com.example.calculadorasleep.domain.sleep.model.Sleep
 import com.example.calculadorasleep.domain.sleep.repository.SleepRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 class SaveSleepUseCase(
     private val repository: SleepRepository
@@ -36,10 +38,11 @@ class DeleteSleepUseCase(
 }
 
 
+//Debatir cuando refrescar el promedio si cada vez que se abra pantalla o mientras se agrege un resgistro nuevo
 class GetSleepSinceUseCase(
     private val repository: SleepRepository
 ) {
-    suspend operator fun invoke(dias: Int): List<Sleep> {
+    suspend operator fun invoke(dias: Int): Flow<List<Sleep>> {
         val desde = System.currentTimeMillis() - (dias * 24 * 60 * 60 * 1000L)
         return repository.getSince(desde)
     }
@@ -59,27 +62,27 @@ class ObserveSleepHistoryUseCase(
 class GetSleepStatsUseCase(
     private val getSleepSince: GetSleepSinceUseCase
 ) {
-    suspend operator fun invoke(dias: Int = 7): SleepStats {
-        val registros = getSleepSince(dias)
+     suspend operator fun invoke(dias: Int = 7): Flow<SleepStats> {
 
-        if (registros.isEmpty()) return SleepStats.vacio()
+        return getSleepSince(dias).map {registros->
+            if (registros.isEmpty()) return@map SleepStats.vacio()
 
-        val duracionPromedioMin = registros.map {
-            (it.despertarTiempo - it.dormirTiempo) / 60000
-        }.average().toInt()
+            val duracionPromedioMin=registros.map {
+                (it.despertarTiempo-it.dormirTiempo)/ 6000
+            }.average().toInt()
 
-        val ciclosPromedio = registros.map { it.ciclos }.average()
+            val ciclosPromedio=registros.map { it.ciclos }.average()
+            val calidadPromedio = registros.mapNotNull { it.calidadSleep }
+                .takeIf { it.isNotEmpty() }
+                ?.average()
 
-        val calidadPromedio = registros.mapNotNull { it.calidadSleep }
-            .takeIf { it.isNotEmpty() }
-            ?.average()
-
-        return SleepStats(
+        SleepStats(
             noches = registros.size,
             duracionPromedioMin = duracionPromedioMin,
             ciclosPromedio = ciclosPromedio,
             calidadPromedio = calidadPromedio
         )
+        }
     }
 }
 data class SleepStats(
