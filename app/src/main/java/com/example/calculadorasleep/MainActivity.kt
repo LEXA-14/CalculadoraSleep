7a6cf485f5a54a6530978ff0a870264f1134d2d0
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-08-11 11:35:11 -0400

    Feat: Agrega navigation a MainActivity.kt

--- app/src/main/java/com/example/calculadorasleep/MainActivity.kt
+++ app/src/main/java/com/example/calculadorasleep/MainActivity.kt
@@ -1,47 +1,42 @@
 package com.example.calculadorasleep
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation3.runtime.rememberNavBackStack
+import com.example.calculadorasleep.navegacion.AppNavigation
+import com.example.calculadorasleep.navegacion.Screen
 import com.example.calculadorasleep.ui.theme.CalculadoraSleepTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CalculadoraSleepTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val backStack = rememberNavBackStack(Screen.Home)
+                    AppNavigation(backStack = backStack)
+
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculadoraSleepTheme {
-        Greeting("Android")
-    }
-}
