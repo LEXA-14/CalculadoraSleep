d73469498b0c5c2a8d741262e7841091c6975e7e
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-08-10 20:12:27 -0400

    Feat: Crea LoginUiEvent para el manejo de eventos de login

---
+++ app/src/main/java/com/example/calculadorasleep/presentation/auth/login/LoginUiEvent.kt
@@ -0,0 +1,9 @@
+package com.example.calculadorasleep.presentation.auth.login
+
+sealed class LoginUiEvent {
+    data class EmailChanged(val value: String) : LoginUiEvent()
+    data class PasswordChanged(val value: String) : LoginUiEvent()
+    object LoginSubmit : LoginUiEvent()
+    data class GoogleSignInSubmit(val idToken: String) : LoginUiEvent()
+    object ClearError : LoginUiEvent()
+}
