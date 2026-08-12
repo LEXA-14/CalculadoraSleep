fafbfcad614fea97505659003767550cbc09a33b
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-08-12 04:12:35 -0400

    Fix: Actualiza la ruta de importación de SleepDao tras cambio de paquete

--- app/src/main/java/com/example/calculadorasleep/data/sleep/repository/sleepRepoImpl.kt
+++ app/src/main/java/com/example/calculadorasleep/data/sleep/repository/sleepRepoImpl.kt
@@ -1,7 +1,7 @@
 package com.example.calculadorasleep.data.sleep.repository
 
 import coil.decode.DataSource
-import com.example.calculadorasleep.data.sleep.local.SleepDao
+import com.example.calculadorasleep.data.sleep.local.sleep.SleepDao
 import com.example.calculadorasleep.data.sleep.mapper.toDomain
 import com.example.calculadorasleep.data.sleep.mapper.toEntity
 import com.example.calculadorasleep.domain.sleep.model.Sleep
