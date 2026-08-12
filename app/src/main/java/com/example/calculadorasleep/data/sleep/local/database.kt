7faa610984b220b5eba44fd9a9df825e3648c76b
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-08-12 03:57:54 -0400

    Feat: Agrega AlarmEntity y AlarmDao a la base de datos

--- app/src/main/java/com/example/calculadorasleep/data/sleep/local/database.kt
+++ app/src/main/java/com/example/calculadorasleep/data/sleep/local/database.kt
@@ -3,12 +3,21 @@ package com.example.calculadorasleep.data.sleep.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.calculadorasleep.data.sleep.local.alarm.AlarmDao
+import com.example.calculadorasleep.data.sleep.local.alarm.AlarmEntity
+import com.example.calculadorasleep.data.sleep.local.sleep.SleepDao
+import com.example.calculadorasleep.data.sleep.local.sleep.SleepEntity
 
-@Database(entities = [SleepEntity::class],
-    version = 1,
+@Database(entities = [
+    SleepEntity::class,
+    AlarmEntity::class
+    ],
+    version = 2,
     exportSchema = false)
 abstract class SleepDatabase : RoomDatabase() {
 
     abstract fun sleepDao(): SleepDao
+
+    abstract fun alarmDao() : AlarmDao
 
 }
