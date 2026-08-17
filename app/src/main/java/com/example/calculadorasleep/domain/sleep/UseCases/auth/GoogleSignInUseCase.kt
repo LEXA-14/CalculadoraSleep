0167e12da70bb794c6967cc76f7101412735221a
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-08-17 19:28:24 -0400

    Refactor: Cambia el parámetro idToken por Context en GoogleSignInUseCase

--- app/src/main/java/com/example/calculadorasleep/domain/sleep/UseCases/auth/GoogleSignInUseCase.kt
+++ app/src/main/java/com/example/calculadorasleep/domain/sleep/UseCases/auth/GoogleSignInUseCase.kt
@@ -1,15 +1,13 @@
 package com.example.calculadorasleep.domain.sleep.UseCases.auth
 
+import android.content.Context
 import com.example.calculadorasleep.domain.sleep.repository.AuthRepository
 import javax.inject.Inject
 
 class GoogleSignInUseCase @Inject constructor(
     private val repository: AuthRepository
 ) {
-    suspend operator fun invoke(idToken: String): Result<Boolean> {
-        if (idToken.isBlank()) {
-            return Result.failure(Exception("Token de Google inválido"))
-        }
-        return repository.signInWithGoogle(idToken)
+    suspend operator fun invoke(context: Context): Result<Boolean> {
+        return repository.signInWithGoogle(context)
     }
 }
