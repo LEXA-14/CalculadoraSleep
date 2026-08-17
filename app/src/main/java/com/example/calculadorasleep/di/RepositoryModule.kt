4719b9cac5209a15fc68994f2ef05420b08b1ffa
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-08-17 19:26:56 -0400

    Feat: Provee CredentialManager en RepositoryModule

--- app/src/main/java/com/example/calculadorasleep/di/RepositoryModule.kt
+++ app/src/main/java/com/example/calculadorasleep/di/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.example.calculadorasleep.di
 
+import android.content.Context
+import androidx.credentials.CredentialManager
 import com.example.calculadorasleep.data.sleep.repository.AlarmRepositoryImpl
 import com.example.calculadorasleep.data.sleep.repository.AuthRepositoryImpl
 import com.example.calculadorasleep.data.sleep.repository.SleepRepositoryImpl
@@ -11,6 +13,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -41,6 +44,12 @@ abstract class RepositoryModule {
         @Singleton
         fun provideFirebaseAuth(): FirebaseAuth {
             return FirebaseAuth.getInstance()
+        }
+
+        @Provides
+        @Singleton
+        fun provideCredentialManager(@ApplicationContext context: Context): CredentialManager {
+            return CredentialManager.create(context)
         }
     }
 }
