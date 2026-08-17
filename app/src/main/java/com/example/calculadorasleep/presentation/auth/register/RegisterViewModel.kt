9688298bef8171cfa848a6b24c0fef15fdf971fb
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-08-17 19:26:31 -0400

    Refactor: Pasa el context al GoogleSignInUseCase en el RegisterViewModel

--- app/src/main/java/com/example/calculadorasleep/presentation/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/calculadorasleep/presentation/auth/register/RegisterViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.calculadorasleep.presentation.auth.register
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.calculadorasleep.domain.sleep.UseCases.auth.GoogleSignInUseCase
@@ -26,7 +27,7 @@ class RegisterViewModel @Inject constructor(
             is RegisterUiEvent.EmailChanged -> _state.update { it.copy(email = event.value, error = null) }
             is RegisterUiEvent.PasswordChanged -> _state.update { it.copy(password = event.value, error = null) }
             RegisterUiEvent.RegisterSubmit -> registerWithEmail()
-            is RegisterUiEvent.GoogleSignInSubmit -> loginWithGoogle(event.idToken)
+            is RegisterUiEvent.GoogleSignInSubmit -> loginWithGoogle(event.context)
             RegisterUiEvent.ClearError -> _state.update { it.copy(error = null) }
         }
     }
@@ -53,10 +54,10 @@ class RegisterViewModel @Inject constructor(
         }
     }
 
-    private fun loginWithGoogle(idToken: String) {
+    private fun loginWithGoogle(context: Context) {
         viewModelScope.launch {
             _state.update { it.copy(isLoading = true, error = null) }
-            val result = googleSignInUseCase(idToken)
+            val result = googleSignInUseCase(context)
             result.onSuccess {
                 _state.update { it.copy(isLoading = false, isSuccess = true) }
             }.onFailure { error ->
